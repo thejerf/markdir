bbde405e2d83a5e0e27aed5d36d0c05b409de94e
xuyundong <xuyundong@douban.com>
2019-07-26 09:19:15 +0800

    Feat: add contentroot and coderoot

---
+++ path_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetCodeRoot(t *testing.T) {
+	path, err := GetCodeRoot()
+
+	if err != nil {
+		t.Errorf("err %v != nil", err)
+	}
+
+	pwd, _ := os.Getwd()
+	if path != pwd {
+		t.Errorf("path %v != %v", path, pwd)
+	}
+}
+
+
+func TestGetCodeRootWithVariable(t *testing.T) {
+	targetPath := "/tmp"
+
+	err := os.Setenv("MARKDIR_CODE_ROOT", targetPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	path, err := GetCodeRoot()
+	if err != nil {
+		t.Errorf("err %v != nil", err)
+	}
+
+	if path != targetPath {
+		t.Errorf("path %v != %v", path, targetPath)
+	}
+}
