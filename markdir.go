6138b06924d7fa66dd4265b1fd2dcbe8f20ec733
bwangelme <bwangel.me@gmail.com>
2019-07-28 20:26:09 +0800

    Fix: fix the path error

--- markdir.go
+++ markdir.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,49 +18,52 @@ import (
 var (
 	bind        = flag.String("bind", "127.0.0.1:8080", "port to run the server on")
 	contentRoot = flag.String("root", ".", "markdown files root dir")
-	is_packr    = true
+	isPackr     = true
 )
 
 type httpHandler struct {
-	contentRoot http.Dir
+	contentRoot string
 	fileServer  http.Handler
 }
 
-func NewDefaultHandler(root http.Dir) http.Handler {
+func NewDefaultHandler(root string) http.Handler {
 	return &httpHandler{
 		contentRoot: root,
-		fileServer:  http.FileServer(root),
+		fileServer:  http.FileServer(http.Dir(root)),
 	}
 }
 
-func getTemplate(tmplName string, debug bool) (*template.Template, error) {
-	if debug {
-		name := filepath.Join("templates", tmplName)
-		return template.ParseFiles(name)
-	} else {
+func getTemplate(tmplName string, isPackr bool) (*template.Template, error) {
+	if isPackr {
 		templateBox := packr.New("template", "./templates")
 		baseHTML, err := templateBox.FindString(tmplName)
 		if err != nil {
 			return nil, err
 		}
 		return template.New("base").Parse(baseHTML)
+	} else {
+		name := filepath.Join("templates", tmplName)
+		return template.ParseFiles(name)
 	}
 }
 
 func (r *httpHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var filename string
 	upath := req.URL.Path
 
 	isMarkdownFile := strings.HasSuffix(upath, ".md") || strings.HasSuffix(upath, ".markdown")
 	if !isMarkdownFile {
 		var exist bool
-		upath, exist = getREADMEPath(upath, string(r.contentRoot))
+		filename, exist = getREADMEPath(upath, r.contentRoot)
 		if !exist {
 			r.fileServer.ServeHTTP(w, req)
 			return
 		}
+	} else {
+		filename = filepath.Join(r.contentRoot, upath)
 	}
 
-	input, err := ioutil.ReadFile(upath)
+	input, err := ioutil.ReadFile(filename)
 	if err != nil {
 		msg := fmt.Sprintf("Couldn't read path %s: %v", req.URL.Path, err)
 		http.Error(w, msg, http.StatusNotFound)
@@ -69,7 +73,7 @@ func (r *httpHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	output := MarkDown(input)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	outputTemplate, err := getTemplate("base.html", is_packr)
+	outputTemplate, err := getTemplate("base.html", isPackr)
 	if err != nil {
 		msg := fmt.Sprintf("err: %v", err)
 		http.Error(w, msg, http.StatusInternalServerError)
@@ -108,7 +112,12 @@ func getREADMEPath(upath string, contentRoot string) (string, bool) {
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s\n", r.Method, r.URL)
+		urlStr := fmt.Sprintf("%v", r.URL)
+		path, err := url.QueryUnescape(urlStr)
+		if err != nil {
+			path = urlStr
+		}
+		log.Printf("%s %s\n", r.Method, path)
 		handler.ServeHTTP(w, r)
 	})
 }
@@ -120,21 +129,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	markdownDir := http.Dir(root)
-	defaultHandler := NewDefaultHandler(markdownDir)
+	defaultHandler := NewDefaultHandler(root)
 
 	mux := http.NewServeMux()
 	var staticHandler http.Handler
-	if is_packr {
-		staticHandler = http.FileServer(http.Dir("static"))
-	} else {
+	if isPackr {
 		staticBox := packr.New("static", "./static")
 		staticHandler = http.FileServer(staticBox)
+	} else {
+		staticHandler = http.FileServer(http.Dir("static"))
 	}
 	mux.Handle("/favicon.ico", staticHandler)
 	mux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 	mux.Handle("/", defaultHandler)
 
-	fmt.Printf("Serving %v on %v\n", *contentRoot, *bind)
+	fmt.Printf("Serving %v on %v\n", root, *bind)
 	log.Fatal(http.ListenAndServe(*bind, logRequest(mux)))
 }
